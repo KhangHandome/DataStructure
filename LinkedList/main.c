cb0e0378db4b2ba5ec9ec29926ae6b3dc5c61ad3
KhangMT <maithanhkhang99@gmail.com>
2025-08-15 13:51:08 +0700

    Update code

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -30,12 +30,66 @@ typedef struct
 static void AddToFirstNode(LinkedList **myList);
 static void AddToEndNode(LinkedList **myList);
 static void ShowAllElementInNode(LinkedList *myList);
-static void ShowElementByCondition(LinkedList **myList);
+static void SortElementByCondition(LinkedList **myList);
 static void DeleteAnElementByIndex(LinkedList **myList);
 int main()
 {
-
+    LinkedList* myStudent = NULL;
     printf("This code is an example about how to used a LinkedList in C \n");
     printf("It was creare by MaiThanhKhang \n");
+    AddToEndNode(&myStudent);
+    AddToEndNode(&myStudent);
+    AddToEndNode(&myStudent);
+    AddToEndNode(&myStudent);
+    ShowAllElementInNode(myStudent);
+
+}
+
+static void AddToEndNode(LinkedList **myList)
+{
+    LinkedList *newNode = NULL;
+    Data_t newData ;
+    uint8_t c = 0 ;
+    printf("Press the Student ID    : ");
+    fgets(newData.StudentID, sizeof(newData.StudentID), stdin);
+    printf("Press the Student name  : ");
+    fgets(newData.StudentName, sizeof(newData.StudentName), stdin);
+    printf("Press the Math Score    : ");
+    scanf("%f",&newData.MathScore);
+    printf("Press the Poet Score    : ");
+    scanf("%f",&newData.PoetSocre);
+    printf("Press the English Score : ");
+    scanf("%f",&newData.EnglishScore);
+    while ((c = getchar()) != '\n' && c != EOF) { }
+    newData.AverageScore = (newData.MathScore + newData.EnglishScore + newData.PoetSocre) / 3 ; 
+    newNode = (LinkedList *) malloc( sizeof(LinkedList));
+    newNode->myStudent = newData;
+    if(*myList == NULL)
+    {
+        printf("There is no element in the node, creating a new node \n");
+        newNode->next = NULL;
+        (*myList) = newNode;
+    }
+    else
+    {
+        printf("Adding a new alement in to the node \n");
+        newNode->next = (struct LinkedList*)(*myList);
+        (*myList) = newNode;
+    }
+}
+static void ShowAllElementInNode(LinkedList *myList)
+{
+    printf("Export all data in node to the screen \n");
+    do
+    {
+        printf("Student ID   : %s",myList->myStudent.StudentID);
+        printf("Student name : %s",myList->myStudent.StudentName);
+        printf("Math Score   : %.2f\n",myList->myStudent.MathScore);
+        printf("Poet Score   : %.2f\n",myList->myStudent.PoetSocre);
+        printf("English Score: %.2f\n",myList->myStudent.EnglishScore);
+        printf("Average Score: %.2f\n",myList->myStudent.AverageScore);
+        myList = (LinkedList*) myList->next;
+    }
+    while(myList != NULL);
 
 }
